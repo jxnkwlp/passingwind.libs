668df178c3a310258abb33bfd2fe26733c644d12
Passingwind <jxnkwlp@qq.com>
2019-07-11 09:26:30 +0800

    first commit

---
+++ Passingwind.PagedList/Passingwind.PagedList/IPagedList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Passingwind.PagedList
+{
+    public interface IPagedList
+    {
+        int PageSize { get; set; }
+
+        int PageNumber { get; set; }
+
+        int TotalPage { get; }
+
+        int TotalCount { get; }
+    }
+
+    public interface IPagedList<T> : IList<T>, IPagedList
+    {
+        IPagedList<T2> ReplaceTo<T2>(Func<T, T2> func); 
+    }
+}
---
+++ Passingwind.PagedList/Passingwind.PagedList/PagedListExtensions.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Passingwind.PagedList
+{
+	/// <summary>
+	///  pagedlist extensions
+	/// </summary>
+	public static class PagedListExtensions
+	{
+		/// <summary>
+		///  convent list to paged list
+		/// </summary> 
+		public static IPagedList<T> ToPagedList<T>(this IEnumerable<T> list, int pageNumber, int pageSize)
+		{
+			var result = new PagedList<T>();
+			result.LoadSource(list, pageNumber, pageSize);
+			return result;
+		}
+
+		/// <summary>
+		///  convent list to paged list
+		/// </summary> 
+		public static IPagedList<T> ToPagedList<T>(this IEnumerable<T> list, int pageNumber, int pageSize, int totalCount)
+		{
+			var result = new PagedList<T>();
+			result.LoadSource(list, pageNumber, pageSize, totalCount);
+			return result;
+		}
+
+		/// <summary>
+		///  convent list to paged list
+		/// </summary> 
+		public static IPagedList<T> ToPagedList<T>(this IList<T> list, int pageNumber, int pageSize)
+		{
+			var result = new PagedList<T>();
+			result.LoadSource(list, pageNumber, pageSize);
+			return result;
+		}
+
+		/// <summary>
+		///  convent list to paged list
+		/// </summary> 
+		public static IPagedList<T> ToPagedList<T>(this IList<T> list, int pageNumber, int pageSize, int totalCount)
+		{
+			var result = new PagedList<T>();
+			result.LoadSource(list, pageNumber, pageSize, totalCount);
+			return result;
+		}
+
+		/// <summary>
+		///  convent list to paged list
+		/// </summary> 
+		public static IPagedList<T> ToPagedList<T>(this IQueryable<T> list, int pageNumber, int pageSize)
+		{
+			var result = new PagedList<T>();
+			result.LoadSource(list, pageNumber, pageSize);
+			return result;
+		}
+
+		/// <summary>
+		///  convent list to paged list
+		/// </summary> 
+		public static IPagedList<T> ToPagedList<T>(this IOrderedQueryable<T> list, int pageNumber, int pageSize)
+		{
+			var result = new PagedList<T>();
+			result.LoadSource(list, pageNumber, pageSize);
+			return result;
+		}
+	}
+}
